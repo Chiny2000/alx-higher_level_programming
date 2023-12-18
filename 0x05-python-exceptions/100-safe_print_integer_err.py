8bc014202a3448e3292ffee3b6843958e3903e7f
Chiny2000 <enocho460@gmail.com>
2023-12-18 06:11:31 -0800

    7. Safe integer print with error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+import sys
 
-def raise_exception_msg(message=""):
-    raise NameError(message)
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as e:
+        print("Exception: " + str(e), file=sys.stderr)
+        return False
+    return True
