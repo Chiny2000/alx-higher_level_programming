cc1503da9145e2f123e2d6caa393d5e84de18a64
Chiny2000 <enocho460@gmail.com>
2024-03-13 07:31:18 -0700

    11. Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+const { list } = require('./100-data');
 
-const newList = list.map((val, idx) => val * idx);
+const mappedList = list.map((value, index) => value * index);
+
 console.log(list);
-console.log(newList);
+console.log(mappedList);
